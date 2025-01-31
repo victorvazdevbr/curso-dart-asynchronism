ff723a034b0f7ed63e020d81248bac6cbad236e8
Victor Vaz <contato@victorvaz.dev.br>
2025-01-31 11:08:49 -0300

    Envio de informações à API Gist desenvolvida

--- bin/dart_asynchronism.dart
+++ bin/dart_asynchronism.dart
@@ -1,9 +1,16 @@
+import 'package:dart_asynchronism/api_key.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert' as convert;
 
 void main() {
   // print('Hello, World!');
-  requestDataAsync();
+  // requestDataAsync();
+  sendDataAsync({
+    'id': 'NEW001',
+    'name': 'flutter',
+    'lastName': 'Dart',
+    'balance': 5000,
+  });
 }
 
 void requestData() {
@@ -30,13 +37,35 @@ void requestData() {
   print('Última coisa a acontecer na função.');
 }
 
-Future<void> requestDataAsync() async {
+Future<List<dynamic>> requestDataAsync() async {
   String url =
       'https://gist.githubusercontent.com/victorvazlabs/6d77af408b596ad4eab5da983949fc02/raw/f587f8cadfadea10030bebbdc19fd4ca6bc2cce0/accounts.json';
 
-  http.Response response = await http.get(Uri.parse(url));
+  final http.Response response = await http.get(Uri.parse(url));
 
-  print(convert.jsonDecode(response.body)[0]);
+  return convert.jsonDecode(response.body);
+}
 
-  print('De fato a última coisa vai acontecer na funcão.');
+Future<void> sendDataAsync(Map<String, dynamic> mapAccount) async {
+  List<dynamic> listAccounts = await requestDataAsync();
+
+  listAccounts.add(mapAccount);
+  String content = convert.jsonEncode(listAccounts);
+
+  String url = 'https://api.github.com/gists/6d77af408b596ad4eab5da983949fc02';
+
+  http.Response response = await http.post(
+    Uri.parse(url),
+    headers: {'Authorization': 'Bearer $githubApiKey'},
+    body: convert.jsonEncode({
+      'description': 'accounts.json',
+      'public': true,
+      'files': {
+        'accounts.json': {
+          'content': content,
+        }
+      }
+    }),
+  );
+  print(response.statusCode);
 }
